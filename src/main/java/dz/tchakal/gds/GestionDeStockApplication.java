127672f5651d0997e6c103546f79bf1a9c3aea93
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-14 10:26:10 +0100

    Création du projet avec le min du configuration

---
+++ src/main/java/dz/tchakal/gds/GestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package dz.tchakal.gds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dz/tchakal/gds/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.tchakal.gds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
