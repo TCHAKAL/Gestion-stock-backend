defbea193c08995070b882eb98412ab53a26f2b5
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-15 14:02:21 +0100

    Création des handler pour gérer les exceptions

---
+++ src/main/java/dz/tchakal/gds/handler/ErrorDto.java
@@ -0,0 +1,23 @@
+package dz.tchakal.gds.handler;
+
+import dz.tchakal.gds.exception.ErrorCode;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer httpCode;
+
+    private ErrorCode errorCode;
+
+    private String message;
+
+    private List<String> errors = new ArrayList<>();
+
+}
